123fbcc04f99c876737c5148fbe1d632ec46f718
joshikommaraju <67496196+joshikommaraju@users.noreply.github.com>
2020-11-16 00:44:09 +0530

    Add files via upload

---
+++ precache-manifest.0889f4d25c87b1995a02b17ae905a292.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f727d82e2d67389b1e03",
+    "url": "/static/css/main.6fb45695.chunk.css"
+  },
+  {
+    "revision": "f727d82e2d67389b1e03",
+    "url": "/static/js/main.f727d82e.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "3721cb977ead0351596c",
+    "url": "/static/js/2.3721cb97.chunk.js"
+  },
+  {
+    "revision": "2359ef3224883c9017d0a1fe7ff2f010",
+    "url": "/index.html"
+  }
+];
